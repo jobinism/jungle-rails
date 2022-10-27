38f4af05e45a91b268d33a097902840bdd61a67c
Jarret Coyle <jarretcoyletoo@gmail.com>
2022-10-26 21:44:05 -0600

    updated user model, sessions controller, and wrote out tests for user auth, realize I don't have login or register in the nav bar, but will update that on the next go

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,16 @@ class User < ApplicationRecord
 
   has_secure_password
 
+
+  #how cool is the validates method?!
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 8 }
+  validates :password_confirmation, presence: true
+
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by(email: email.strip.downcase)
+    return user if user && user.authenticate(password)
+  end
+
 end
